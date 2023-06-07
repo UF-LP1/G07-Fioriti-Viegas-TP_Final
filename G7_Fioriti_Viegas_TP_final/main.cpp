3c5cfe06d08e7e64e9ea06fe1bca52d4dff57a73
Fedefioriti <ffioriti10@gmail.com>
2023-06-07 18:32:40 -0300

    main

---
+++ G7_Fioriti_Viegas_TP_final/main.cpp
@@ -0,0 +1,31 @@
+
+#include "CBSA.H"
+
+int main() {
+	Ccentro_salud Favaloro("Hospital Favaloro", "necochea_163", "CABA", CABA, "17262537832");
+	Ccentro_salud mendoza("Clinica Mendo", "necochea_163", "CABA", Mendoza, "73246638859");
+	Ccentro_salud BsAs("Hospital Favaloro", "necochea_163", "CABA", BsAs, "76834672372"); 
+	Ccentro_salud chubut("Hospital Favaloro", "necochea_163", "CABA", Chubut, "87457867232"); 
+	Ccentro_salud sanJuan("Hospital Favaloro", "necochea_163", "CABA", San_Juan, "23432679732"); 
+	Ccentro_salud sanLuis("Hospital Favaloro", "necochea_163", "CABA", San_Luis, "78362875623"); 
+	Ccentro_salud cordoba("Hospital Favaloro", "necochea_163", "CABA", Cordoba, "87231566812"); 
+	Ccentro_salud misiones("Hospital Favaloro", "necochea_163", "CABA", Misiones, "87236426344"); 
+	Ccentro_salud jujuy("Hospital Favaloro", "necochea_163", "CABA", Jujuy, "87623486265"); 
+	Ccentro_salud santiagoDelEstero("Hospital Favaloro", "necochea_163", "CABA", Santiago_del_Estero, "12376736434"); 
+	Ccentro_salud RioNegro("Hospital Favaloro", "necochea_163", "CABA", Rio_Negro, "89726345323");
+
+	vector<Ccentro_salud*> centros_salud;
+	centros_salud.push_back(&Favaloro);
+	centros_salud.push_back(&mendoza);
+	centros_salud.push_back(&BsAs);
+	centros_salud.push_back(&chubut);
+	centros_salud.push_back(&sanJuan);
+	centros_salud.push_back(&sanLuis);
+	centros_salud.push_back(&cordoba);
+	centros_salud.push_back(&misiones);
+	centros_salud.push_back(&jujuy);
+	centros_salud.push_back(&santiagoDelEstero);
+	centros_salud.push_back(&RioNegro);
+
+	CBSA BSA(centros_salud);
+}
